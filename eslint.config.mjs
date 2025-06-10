5f596894972f2f4a303c4f823d5c8c45023a8e50
Fabian Beltran <46061339+FabianBeltran96@users.noreply.github.com>
2025-06-10 17:07:46 -0500

    chore: install development dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-plugin-prettier'
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }
+)
